0de5312c351f0a7088d24df69a56908aa7b8ab74
NeverQuitLearning <645969568@qq.com>
2026-06-02 09:12:46 +0800

    🚀 天空观测助手 v1：火烧云/晚霞/彩虹预测 + 拍摄地点 + 智能拍摄指南

---
+++ sw.js
@@ -0,0 +1,16 @@
+const CACHE = 'sky-watcher-v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll([
+    '/sky-watcher.html',
+    '/manifest.json',
+    '/icon-192.svg',
+    '/icon-512.svg'
+  ])));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => e.waitUntil(clients.claim()));
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).catch(() => new Response('', {status: 200})))
+  );
+});
